26907fedb8e8fa6c24c19795513b1a570025483f
huisheng <huisheng@ximnet.com.my>
2022-12-30 19:37:01 +0800

    wip

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import CardList from './components/card-list/card-list.component'
 
 class App extends Component {
 	constructor() {
@@ -80,6 +81,7 @@ class App extends Component {
 						</div>
 					)
 				})}
+				<CardList />
 			</div>
 		)
 	}
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,8 @@
+import { Component } from 'react'
+class CardList extends Component {
+	render() {
+		return <div>I'm card list</div>
+	}
+}
+
+export default CardList
