238911a7caca4c30da43b48449a3b65addbf2c33
huisheng <huisheng@ximnet.com.my>
2022-12-30 13:20:04 +0800

    wip

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
 
 		this.state = {
 			monsters: [],
+			searchField: '',
 		}
 
 		console.log('constructor')
@@ -48,14 +49,25 @@ class App extends Component {
 	render() {
 		console.log('render')
 
+		const filteredMonsters = this.state.monsters.filter((monster) => {
+			return monster.name.toLocaleLowerCase().includes(this.state.searchField)
+		})
+
 		return (
 			<div className='App'>
 				<input
 					type='search'
 					className='search-box'
 					placeholder='search monsters'
+					onChange={(event) => {
+						const searchField = event.target.value.toLocaleLowerCase()
+
+						this.setState(() => {
+							return { searchField }
+						})
+					}}
 				/>
-				{this.state.monsters.map((monster) => {
+				{filteredMonsters.map((monster) => {
 					return (
 						<div key={monster.id}>
 							<h1>{monster.name}</h1>
