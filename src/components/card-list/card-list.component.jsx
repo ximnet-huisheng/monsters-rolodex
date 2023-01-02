b6e0d9e86a73490ec5b8b53c0777cae6a1c9b262
huisheng <huisheng@ximnet.com.my>
2023-01-02 13:48:09 +0800

    wip

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,19 +1,11 @@
-import { Component } from 'react'
 import Card from '../card/card.component'
 import './card-list.styles.css'
-class CardList extends Component {
-	render() {
-		const { monsters } = this.props
-		console.log('render from card list')
-
-		return (
-			<div className='card-list'>
-				{monsters.map((monster) => {
-					return <Card monster={monster} />
-				})}
-			</div>
-		)
-	}
-}
+const CardList = ({ monsters }) => (
+	<div className='card-list'>
+		{monsters.map((monster) => {
+			return <Card monster={monster} />
+		})}
+	</div>
+)
 
 export default CardList
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,18 +1,13 @@
-import { Component } from 'react'
 import './search-box.styles.css'
-class SearchBox extends Component {
-	render() {
-		const { className, placeholder, onSearchHandler } = this.props
-
-		return (
-			<input
-				type='search'
-				className={`search-box ${className}`}
-				placeholder={placeholder}
-				onChange={onSearchHandler}
-			/>
-		)
-	}
+const SearchBox = ({ className, placeholder, onSearchHandler }) => {
+	return (
+		<input
+			type='search'
+			className={`search-box ${className}`}
+			placeholder={placeholder}
+			onChange={onSearchHandler}
+		/>
+	)
 }
 
 export default SearchBox
